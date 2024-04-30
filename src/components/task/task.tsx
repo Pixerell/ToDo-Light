793dc231cf903d8c73d55ee9efc4cc39ef02d69b
Pixerell <PinkDriftsA@protonmail.com>
2024-04-30 18:23:57 +0300

    markup, adaptivity

--- src/components/task/task.tsx
+++ src/components/task/task.tsx
@@ -1,1 +1,38 @@
-export {}
+import React from 'react'
+import  './task.css'
+import '../../App.scss'
+import circle from '../../assets/circle.svg'
+import like from '../../assets/like.svg'
+import logo from '../../assets/appIco.svg'
+
+interface TaskItemProps {
+    taskDescription: string;
+    date: string;
+    status: string;
+}
+
+function TaskItem({ taskDescription, date, status }: TaskItemProps) {
+    const getIcon = () => {
+        switch (status) {
+            case 'Pending':
+                return circle;
+            case 'Completed':
+                return like;
+            default:
+                return logo;
+        }
+    };
+
+    const iconSrc = getIcon();
+    const textColor = status === 'Completed' ? 'var(--cyan-color)' : 'var(--yellow-color)';
+
+    return (
+        <div className="taskItem" style={{ color: textColor }}>
+            <img className="imgAnim" src={iconSrc} alt={`${status} Icon`}/>
+            <p className="taskDesc" >{taskDescription}</p>
+            <p className="taskDate">{date}</p>
+        </div>
+    )
+}
+
+export default TaskItem;
